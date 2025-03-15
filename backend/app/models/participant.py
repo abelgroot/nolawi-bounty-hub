3a2bd85ea94db8cc2772c79a36dcc4febba2da05
iam61007 <105054470+abelgroot@users.noreply.github.com>
2025-03-15 17:31:50 +0300

    update participant

--- backend/app/models/participant.py
+++ backend/app/models/participant.py
@@ -8,17 +8,17 @@ from sqlmodel import Field, SQLModel
 class Participant(SQLModel, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
-    hacker_id: UUID = Field(default_factory=uuid4, foreign_key="user.id")
-    program_id: UUID = Field(default_factory=uuid4, foreign_key="bountyprogram.id")
+    hacker_id: UUID = Field(foreign_key="user.id")
+    program_id: UUID = Field(foreign_key="bountyprogram.id")
 
 
 class ParticipantCreate(BaseModel):
-    hacker_id: UUID = Field(default_factory=uuid4, foreign_key="user.id")
-    program_id: UUID = Field(default_factory=uuid4, foreign_key="bountyprogram.id")
+    hacker_id: UUID = Field(foreign_key="user.id")
+    program_id: UUID = Field(foreign_key="bountyprogram.id")
 
 
 class ParticipantRead(SQLModel):
     id: UUID
-    hacker_id: UUID = Field(default_factory=uuid4, foreign_key="user.id")
-    program_id: UUID = Field(default_factory=uuid4, foreign_key="bountyprogram.id")
+    hacker_id: UUID = Field(foreign_key="user.id")
+    program_id: UUID = Field(foreign_key="bountyprogram.id")
     created_at: datetime
