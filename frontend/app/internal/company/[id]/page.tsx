2f78c8024d10ac84fac493816fd4b35b1ecbafa4
iam61007 <105054470+abelgroot@users.noreply.github.com>
2025-03-22 13:16:06 +0300

    update company page to have a back button

--- frontend/app/internal/company/[id]/page.tsx
+++ frontend/app/internal/company/[id]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { ProgramItem } from "@/components/program";
+import { useParticipation } from "@/hooks/useParticipation";
 import { usePrograms } from "@/hooks/usePrograms";
+import { useSubmissions } from "@/hooks/useSubmissions";
 import { useUser } from "@/hooks/useUser";
 import { useCurrentUser } from "@/providers/auth-provider";
-import { Loader2 } from "lucide-react";
+import { ChevronLeft, Loader2 } from "lucide-react";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function Page() {
@@ -13,12 +16,31 @@ export default function Page() {
 
   const { user } = useUser(id);
   const { user: currentUser } = useCurrentUser();
-  const { programs, isLoading } = usePrograms();
+  const { programs, isLoading: loadingPrograms } = usePrograms();
+
+  const { joinedPrograms, isLoading: loadingJoined } = useParticipation(
+    currentUser?.id,
+  );
+  const { submissions, isLoading: loadingSubmissions } = useSubmissions();
+
+  const joinedProgramIds = joinedPrograms.map((program) => program.id);
+  const availablePrograms = programs.filter(
+    (program) => !joinedProgramIds.includes(program.id),
+  );
+
+  const isLoading = loadingPrograms || loadingJoined || loadingSubmissions;
 
   return (
     <div className="p-10">
       <h2 className="text-xl font-semibold p-4 border-b mb-6 flex justify-between items-center">
-        <div>Campnay ID: {user?.email}</div>
+        <Link
+          href={`/internal/user`}
+          className="flex justify-center items-center gap-2 border rounded-md px-2 py-1 text-sm text-slate-700 hover:bg-slate-100"
+        >
+          <ChevronLeft className="size-4" />
+          Back
+        </Link>
+        <div>Campany : {user?.name}</div>
       </h2>
       <div className="p-4 w-full">
         <h2 className="text-lg font-bold">Programs</h2>
@@ -28,17 +50,42 @@ export default function Page() {
               <Loader2 /> loading programs ...
             </div>
           ) : (
-            user &&
-            programs
+            joinedPrograms
               .filter((program) => program.ownerId == id)
-              .map((program, idx) => (
-                <ProgramItem
-                  key={idx}
-                  userId={user.id}
-                  program={program}
-                  isOwner={program.ownerId === currentUser?.id}
-                />
-              ))
+              .map(
+                (program, idx) =>
+                  currentUser && (
+                    <ProgramItem
+                      key={idx}
+                      program={program}
+                      userId={currentUser.id}
+                      joined={true}
+                      submission={submissions.find(
+                        (submission) => submission.programId === program.id,
+                      )}
+                    />
+                  ),
+              )
+          )}
+        </div>
+        <div className="w-full flex flex-col gap-4 mt-4">
+          {isLoading ? (
+            <div>
+              <Loader2 /> loading programs ...
+            </div>
+          ) : (
+            availablePrograms
+              .filter((program) => program.ownerId == id)
+              .map(
+                (program, idx) =>
+                  currentUser && (
+                    <ProgramItem
+                      key={idx}
+                      program={program}
+                      userId={currentUser.id}
+                    />
+                  ),
+              )
           )}
         </div>
       </div>
