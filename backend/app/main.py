a3ea8a616431cd8dd3c8cfe6fb44ace9bce64f51
iam61007 <105054470+abelgroot@users.noreply.github.com>
2025-07-06 18:27:49 +0300

    main update

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 subapp = FastAPI()
 subapp.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:3000", "http://127.0.0.1", "http://127.0.0.1:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
