a7d8bc28104c2ef93ae234c3795586e304907572
iam61007 <105054470+abelgroot@users.noreply.github.com>
2025-07-09 23:40:08 +0300

    create participant redis cache

--- backend/app/routes/participant_routes.py
+++ backend/app/routes/participant_routes.py
@@ -8,13 +8,30 @@ from app.db import SessionDep
 from app.models.participant import (
     ParticipantCreate,
     ParticipantRead,
+    Participant,
 )
 from app.services.participant_service import ParticipantService
+from app.cache.redis_client import redis_client
+from app.cache.cache_decorator import redis_cache
+from app.models.participant import ParticipantRead
+from app.models.bountyprogram import BountyProgram
 
 participant_router = APIRouter(prefix="/participation", tags=["Participants"])
 
+def hacker_participation_key_builder(hacker_id: UUID, session: SessionDep):
+    return f"participations:hacker:{str(hacker_id)}"
+
+
+def participant_key_builder(participant_id: UUID, session: SessionDep):
+    return f"participant:{str(participant_id)}"
+
+
+async def invalidate_participant_caches(hacker_id: UUID):
+    await redis_client.delete(f"participations:hacker:{str(hacker_id)}")
+
 
 @participant_router.get("/hacker/{hacker_id}")
+@redis_cache(key_builder=hacker_participation_key_builder, ttl=60, model_class=BountyProgram)
 async def get_participation(hacker_id: UUID, session: SessionDep):
     participant_service = ParticipantService(session)
     participants = participant_service.get_participations(hacker_id)
@@ -25,10 +42,12 @@ async def get_participation(hacker_id: UUID, session: SessionDep):
 async def create_participant(participant: ParticipantCreate, session: SessionDep):
     participant_service = ParticipantService(session)
     new_participant = participant_service.create_participant(participant)
+    await invalidate_participant_caches(participant.hacker_id)
     return new_participant
 
 
 @participant_router.get("/{participant_id}")
+@redis_cache(key_builder=participant_key_builder, ttl=60, model_class=Participant)
 async def get_participant(participant_id: UUID, session: SessionDep):
     participant_service = ParticipantService(session)
     participant = participant_service.get_participant(participant_id)
@@ -42,6 +61,8 @@ async def delete_participant(
     session: SessionDep,
 ):
     participant_service = ParticipantService(session)
+    await invalidate_participant_caches(hacker_id)
+
     return participant_service.delete_participant(
         program_id=program_id, hacker_id=hacker_id
     )
