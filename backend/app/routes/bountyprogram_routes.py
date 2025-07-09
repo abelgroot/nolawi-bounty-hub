1f9e8ac10a383e3b309ffcb86e021b6018e5f97c
iam61007 <105054470+abelgroot@users.noreply.github.com>
2025-07-09 23:40:00 +0300

    create bountyprogram redis cache

--- backend/app/routes/bountyprogram_routes.py
+++ backend/app/routes/bountyprogram_routes.py
@@ -1,7 +1,7 @@
-"""Bounty Program Routes"""
-
 import traceback
 from uuid import UUID
+from app.cache.cache_decorator import redis_cache
+from app.cache.redis_client import redis_client
 
 from fastapi import APIRouter, HTTPException, Request, status
 
@@ -16,6 +16,31 @@ from app.services.bountyprogram_service import BountyProgramService
 from app.services.user_service import UserService
 
 bountyprogram_router = APIRouter(prefix="/bountyprograms", tags=["BountyPrograms"])
+
+
+def bountyprograms_key_builder(request: Request, session: SessionDep):
+    user_service = UserService(session)
+    user = user_service.get_current_user(request)
+
+    if not user:
+        return "bountyprograms:public:list"
+
+    if user.role == UserType.ADMIN:
+        return "bountyprograms:admin:list"
+    elif user.role == UserType.COMPANY:
+        return f"bountyprograms:company:{user.id}:list"
+
+    return f"bountyprograms:user:{user.id}:list"
+
+
+def bountyprogram_key_builder(bountyprogram_id: UUID, session: SessionDep):
+    return f"bountyprogram:{str(bountyprogram_id)}"
+
+
+async def invalidate_bountyprogram_caches(owner_id: str | UUID):
+    await redis_client.delete(f"bountyprograms:company:{owner_id}:list")
+    await redis_client.delete("bountyprograms:admin:list")
+    await redis_client.delete(f"bountyprograms:user:{owner_id}:list")
 
 
 @bountyprogram_router.post("/", response_model=BountyProgramRead)
@@ -34,9 +59,8 @@ async def create_bountyprogram(
             )
 
         bountyprogram_service = BountyProgramService(session)
-        new_bountyprogram = bountyprogram_service.create_bountyprogram(
-            user=user, bountyprogram_create=bountyprogram
-        )
+        new_bountyprogram = bountyprogram_service.create_bountyprogram(user=user, bountyprogram_create=bountyprogram)
+        await invalidate_bountyprogram_caches(user.id)
         return new_bountyprogram
     except Exception:
         print(traceback.format_exc())
@@ -45,12 +69,8 @@ async def create_bountyprogram(
             detail="Your are not authorized to access this resource.",
         )
 
-    bountyprogram_service = BountyProgramService(session)
-    new_bountyprogram = bountyprogram_service.create_bountyprogram(bountyprogram)
-    return new_bountyprogram
-
-
 @bountyprogram_router.get("/")
+@redis_cache(key_builder=bountyprograms_key_builder, ttl=60, model_class=BountyProgramRead)
 async def get_all_bountyprograms(
     request: Request,
     session: SessionDep,
@@ -80,13 +100,12 @@ async def get_all_bountyprograms(
             detail="Your are not authorized to access this resource.",
         )
 
-
 @bountyprogram_router.get("/{bountyprogram_id}")
+@redis_cache(key_builder=bountyprogram_key_builder, ttl=120, model_class=BountyProgramRead)
 async def get_bountyprogram(bountyprogram_id: UUID, session: SessionDep):
     bountyprogram_service = BountyProgramService(session)
     bountyprogram = bountyprogram_service.get_bountyprogram(bountyprogram_id)
     return bountyprogram
-
 
 @bountyprogram_router.post("/{bountyprogram_id}")
 async def update_bountyprogram(
@@ -95,9 +114,11 @@ async def update_bountyprogram(
     session: SessionDep,
 ):
     bountyprogram_service = BountyProgramService(session)
-    bountyprograms = bountyprogram_service.update_bountyprogram(
-        bountyprogram_id, bountyprogram_update
-    )
+
+    program = bountyprogram_service.get_bountyprogram(bountyprogram_id)
+    await invalidate_bountyprogram_caches(program.owner_id)
+
+    bountyprograms = bountyprogram_service.update_bountyprogram(bountyprogram_id, bountyprogram_update)
     return bountyprograms
 
 
@@ -107,4 +128,8 @@ async def delete_bountyprogram(
     session: SessionDep,
 ):
     bountyprogram_service = BountyProgramService(session)
+
+    program = bountyprogram_service.get_bountyprogram(bountyprogram_id)
+    await invalidate_bountyprogram_caches(program.owner_id)
+
     return bountyprogram_service.delete_bountyprogram(bountyprogram_id)
